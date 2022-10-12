52f742e1b90da93fdf5842d4d4f40976edc93557
ali.hamedah <aliali735522@gmail.com>
2022-10-12 11:54:45 +0200

    =settings_main_sidebar

--- app/Models/Teacher.php
+++ app/Models/Teacher.php
@@ -2,10 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Spatie\Translatable\HasTranslations;
 
-class Teacher extends Model
+class Teacher extends Authenticatable
 {
     use HasTranslations;
 
