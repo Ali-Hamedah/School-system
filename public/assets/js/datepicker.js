fcf5d7d7343c8ca14256a3ef23ed4e8a0d1b5bc4
ali.hamedah <aliali735522@gmail.com>
2022-10-15 11:03:19 +0200

    =attendance_report

--- public/assets/js/datepicker.js
+++ public/assets/js/datepicker.js
@@ -4,17 +4,21 @@ Template:  Webmin - Bootstrap 4 & Angular 5 Admin Dashboard Template
 Author: potenzaglobalsolutions.com
 Design and Developed by: potenzaglobalsolutions.com
 
-NOTE: 
+NOTE:
 
 */
 
- (function($){
-  "use strict";
+(function ($) {
+    "use strict";
 // Line Stacked
 
     $('.date-picker-default').datepicker({
-        format: 'mm-dd-yyyy',
+
+        format: 'yyyy-mm-dd',
+        todayBtn: "linked",
+        clearBtn: true,
         autoclose: true,
+        todayHighlight: true,
         orientation: "bottom",
         templates: {
             leftArrow: '<i class="fa fa-angle-left"></i>',
@@ -41,7 +45,7 @@ NOTE:
             rightArrow: '<i class="fa fa-angle-right"></i>'
         }
     });
-    
+
 
     $('#datepicker-inline').datepicker({
         todayHighlight: true,
@@ -107,11 +111,11 @@ NOTE:
         }
     });
 
-var checkin = $('.range-from').datepicker({
-        onRender: function(date) {
+    var checkin = $('.range-from').datepicker({
+        onRender: function (date) {
             return date.valueOf() < now.valueOf() ? 'disabled' : '';
         }
-    }).on('changeDate', function(ev) {
+    }).on('changeDate', function (ev) {
         if (ev.date.valueOf() > checkout.date.valueOf()) {
             var newDate = new Date(ev.date)
             newDate.setDate(newDate.getDate() + 1);
@@ -122,11 +126,11 @@ var checkin = $('.range-from').datepicker({
         $('.range-to')[0].focus();
     }).data('datepicker');
     var checkout = $('.range-to').datepicker({
-        onRender: function(date) {
+        onRender: function (date) {
             return date.valueOf() <= checkin.date.valueOf() ? 'disabled' : '';
         }
-    }).on('changeDate', function(ev) {
+    }).on('changeDate', function (ev) {
         checkout.hide();
     }).data('datepicker');
 
- })(jQuery);
+})(jQuery);
